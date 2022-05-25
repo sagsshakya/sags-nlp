0d9c0988594873e73b06c2aa2af82c22eb6fb34e
sagsshakya <sags.shakya@gmail.com>
2022-05-25 17:03:25 +0545

    added dummy data at the top of dataframe.

--- app.py
+++ app.py
@@ -30,9 +30,10 @@ content_path = join('database', 'content.tsv')
 if not isfile(content_path):
     print(f"No file named {content_path}.\nConsider creating a new file in {content_path}...\n")
     # Blank Dataframe.
-    df_content = pd.DataFrame(columns = "id date text summary".split())
+    df_content = pd.DataFrame({"id" : 0, "date" : "2022-01-01", "text" : "sample text", "summary" : "Sample summary"})
+    df_content.to_csv(content_path, sep = '\t', encoding = "utf-8", index = None)
 else: 
-    df_content = pd.read_csv(join('database', 'content.tsv'), encoding = True, skip_blank_lines = True, delimiter = '\t')
+    df_content = pd.read_csv(content_path, encoding = True, skip_blank_lines = True, delimiter = '\t')
 
 # Create Flask app.
 app = Flask(__name__)
@@ -52,7 +53,7 @@ def summarize():
         # Obtain text from the text area.
         text = request.form['input_text']    # Store it in the database if you want.
         assert len(text) > 0, "No text was inserted."
-        
+
         # Data.
 
         ## Generate Unique ID for the incoming text.
@@ -63,7 +64,7 @@ def summarize():
                 flag = False
 
         date = datetime.now().date().__str__()
-        summary = nan     # TB modified later.
+        summary = nan                            # TB modified later.
 
         # Store info in a dictionary.
         info_dict = {
@@ -75,19 +76,15 @@ def summarize():
 
         df_content = df_content.append(info_dict, ignore_index = True)
 
+
         # Store in a TSV file.
         df_content.to_csv(content_path, encoding = 'utf-8', header = True, index = None, sep = '\t')
 
-
-
-
-        # Generate a unique ID for an input text and store it in a text file.
-        #text_id = write_id_to_file(filepath = id_path)
-
         return redirect("/summarize")
 
-    else:     
-        return render_template("summary.html", )
+    else:
+
+        return render_template("summary.html")
 
 if __name__ == "__main__":
     app.run(debug = True)
