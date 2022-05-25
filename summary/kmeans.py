8260ad32c130b10efdb19038ad23812abaa79e00
sagsshakya <sags.shakya@gmail.com>
2022-05-25 23:21:08 +0545

    Added optimal K using distance betn point and line

--- summary/kmeans.py
+++ summary/kmeans.py
@@ -10,6 +10,7 @@ Description:
 """
 
 from os import makedirs, path
+from typing import List, Tuple
 from sklearn.cluster import KMeans
 from scipy.spatial.distance import cdist
 from scipy.cluster.vq import vq
@@ -23,7 +24,6 @@ class KMeansClustering:
         self.sentence_vectors = sentence_vectors            # Shape: (num_sentences, num_features)
         self.max_iter = max_iter
         self.num_sentences = sentence_vectors.shape[0]
-        self.k_optimal = 5                                  # Find a better way of selecting this.
         
         makedirs(save_dir, exist_ok=True)
         self.save_dir = save_dir
@@ -60,12 +60,34 @@ class KMeansClustering:
         '''
         plt.style.use('classic')
         plt.plot(K, distortions, color = 'steelblue', marker = 'X')
+        plt.plot((K[0], K[-1]), (distortions[0], distortions[-1]), color = 'tomato', alpha = 0.6, linewidth = 1.2, linestyle = 'dotted')
+        
+        # Plot the optimal value of K.
+        k_optimal, distortion_optimal = self.get_optimal_k(K, distortions)
+        plt.scatter([k_optimal], [distortion_optimal], marker = 'D', color = 'red', s = 50)
         plt.xlabel('Number of clusters (k)')
         plt.ylabel('Distortion')
         plt.title('The Elbow Method showing the optimal k')
         plt.savefig(path.join(self.save_dir, 'kmeans_elbow.png'), dpi = 300)
 
-    def get_optimal_k(self, lower_bound: int = 1, upper_bound: int = 10, plot_elbow: bool = False):
+    def _dist_point_line(self, point: Tuple[float], coefficients: Tuple[float]) -> float:
+        """
+        Calculates the perpendicular distance between a 2D point and a line. 
+
+        Args:
+            point (Tuple[float]): co-ordinates of the point. e.g (1.2, 6.5)
+            coefficients (Tuple[float]): coefficients of the straight line equation. e.g 3x + 4y + 5 = 0 --> (3,4,5)
+
+        Returns:
+            float: Perpendicular distance between a 2D point and a line.
+        """        
+        x,y = point
+        a,b,c = coefficients
+        numerator = (a * x) + (b * y) + c
+        denominator = np.sqrt(a**2 + b**2)
+        return abs(numerator / denominator)
+
+    def perform_elbow(self, lower_bound: int = 1, upper_bound: int = 10, plot_elbow: bool = False):
         distortions = []
         K = range(lower_bound, upper_bound + 1)
         for kk in K:
@@ -78,6 +100,28 @@ class KMeansClustering:
             self._plot_elbow(K, distortions)
 
         return K, distortions
+
+    def get_optimal_k(self, K: List[int], distortions: List[float]) -> tuple:
+        # Get optimal K.
+        a = distortions[0] - distortions[-1]
+        b = K[-1] - K[0]
+        c1 = K[0] * distortions[-1]
+        c2 = K[-1] * distortions[0]
+        c = c1  -c2
+
+        distances = []
+        for kk, dist in zip(K, distortions):
+            point = (kk, dist)
+            coeff = (a, b, c)
+            distances.append(self._dist_point_line(point, coeff))
+
+        # Get the index of the minimum value of the perpn distance.
+        index_max = max(range(len(distances)), key=distances.__getitem__)
+
+        k_optimal = K[index_max]
+        distortion_optimal = distortions[index_max]
+
+        return k_optimal, distortion_optimal
 
     def get_summary_sentence_ids(self):
         closest = self._clustering(n_clusters = self.k_optimal, return_closest = True)
